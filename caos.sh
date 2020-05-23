776591a46d1bde4ea561b1a973b392ad404febf6
rafarios <rafael.rios.valera@gmail.com>
2020-05-23 16:28:22 +0200

    Add 11,6,19,7,19,16,9,12,10,16,5,18 - Delete 6,12 - 23/05/2020 16:28:22

--- caos.sh
+++ caos.sh
@@ -2,18 +2,18 @@
 RANDOM=$$
 
 #add content to some files
-for i in `seq 8` ; do
+for i in `seq 12` ; do
 	a=$(($RANDOM%20))
 	echo $a >> $a.txt
 	if [ -z ${text_add} ] ; then
 		text_add="$a"
 	else
-		text_add="${text_add} $a"
+		text_add="${text_add},$a"
 	fi
 done
 
 #delete some files
-for i in `seq 4` ; do
+for i in `seq 2` ; do
 	a=$(($RANDOM%20))
 	rm $a.txt
 	if [ -z ${text_delete} ] ; then
