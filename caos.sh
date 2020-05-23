601ce9a98d8ed586ad7a38e7d4a7d3d3632a0786
rafarios <rafael.rios.valera@gmail.com>
2020-05-23 16:04:09 +0200

    1

---
+++ caos.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RANDOM=$$
+
+#add content to some files
+for i in `seq 10` ; do
+	a=$(($RANDOM%10))
+	echo $a >> $a.txt
+done
+
+#delete some files
+for i in `seq 3` ; do
+	a=$(($RANDOM%10))
+	rm $a.txt
+done
+
+
+git commit 
