fb52e4d0f42af8e4fd6568a0fca034f4d9b8fa8b
rafarios <rafael.rios.valera@gmail.com>
2020-05-23 16:17:07 +0200

    Add  6 4 3 4 3 5 0 6 6 3 - Delete  6 4 3 4 3 5 0 6 6 3 8 - 23/05/2020 16:17:06

--- caos.sh
+++ caos.sh
@@ -5,14 +5,19 @@ RANDOM=$$
 for i in `seq 10` ; do
 	a=$(($RANDOM%10))
 	echo $a >> $a.txt
+	text_add="${text_add} $a"
 done
 
 #delete some files
 for i in `seq 3` ; do
 	a=$(($RANDOM%10))
 	rm $a.txt
+	text_delete="${text_add} $a"
 done
 
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt"
+
 git add .
-git commit -m "${a}"
+git commit -m "Add ${text_add} - Delete ${text_delete} - $dt"
 git push origin master
