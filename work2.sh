ec2ccec5097716fec860393b3bd1f89df755b776
rafarios <rafael.rios.valera@gmail.com>
2020-05-23 16:32:07 +0200

    Adding more stuff

---
+++ work2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+RANDOM=$$
+
+#add content to some files
+for i in `seq 100` ; do
+	a=$(($RANDOM%20))
+	echo $a >> $a.txt
+	if [ -z ${text_add} ] ; then
+		text_add="$a"
+	else
+		text_add="${text_add},$a"
+	fi
+done
+
+git add .
+git commit -m "Adding more stuff"
+git push origin master
